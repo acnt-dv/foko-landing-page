18c6750e0ae2be1b4b983d10c275c630f473cd9e
d.sajedi <d.sajedi@fanap.ir>
2025-02-18 14:19:13 +0330

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}", // Paths to all of your template files
+    ],
+    theme: {
+        extend: {
+            colors: {
+                foko: '#D9D7C9',
+            },
+        },
+    },
+    plugins: [],
+};
