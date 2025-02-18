34c177cf1bbc9a94763282aaa33054d098027c94
d.sajedi <d.sajedi@fanap.ir>
2025-02-18 14:11:06 +0330

    Foko initiated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from "./componenets/Login";
+import Footer from "./componenets/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="flex flex-col items-center justify-center h-screen bg-foko">
+                <Login/>
+                <Footer/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
