525b0c8f689c949af12f9bfe6cda1b666097b9f4
d.sajedi <d.sajedi@fanap.ir>
2025-02-22 14:50:22 +0330

    minor improvements in responsive

--- src/components/FullPageMenu.js
+++ src/components/FullPageMenu.js
@@ -3,7 +3,7 @@ import logo from "../statics/png/foko-logo.png";
 
 const FullPageMenu = ({isOpen, onClose}) => {
     return (<div
-            className={`fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center transition-opacity duration-300 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+            className={`fixed inset-0 bg-black bg-opacity-90 flex flex-col items-center justify-center transition-opacity duration-500 ease-in-out ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
         >
             <button
                 onClick={onClose}
@@ -12,13 +12,13 @@ const FullPageMenu = ({isOpen, onClose}) => {
                 &times;
             </button>
 
-            <div className="flex w-full items-center justify-center">
-                <span className='flex w-1/2 justify-center items-center'>
+            <div className="flex flex-col xl:flex-row w-full items-center justify-center">
+                <span className='flex w-1/2 justify-center items-center m-[50px]'>
                     <img src={logo} alt={'logo'} className='w-[250px] h-[250px] filter brightness-0 invert'/>
                 </span>
 
                 <nav>
-                    <ul className="flex w-1/2 space-x-24 text-center">
+                    <ul className="flex flex-col xl:flex-row w-1/2 space-y-12 xl:space-y-0 xl:space-x-24 text-center m-[50px]">
                         <li className="text-white text-3xl hover:underline"><a href="#home">Home</a></li>
                         <li className="text-white text-3xl hover:underline"><a href="#work">Work</a></li>
                         <li className="text-white text-3xl hover:underline"><a href="#studio">Studio</a></li>
