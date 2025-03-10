4daf5e1e571c36d117e4fc09fcf3fcea8f757993
d.sajedi <acnt.dv@gmail.com>
2025-03-10 17:57:05 +0330

    Responsive improvements

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,12 +8,12 @@ const AboutUs = () => {
             <div className='flex'>
                 <h1 className="text-16 font-rubik block xl:hidden absolute left-[30px] mt-foko">About <br/> FOKO</h1>
                 <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
-                    <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-7/12 self-end xl:max-w-[450px]">FOKO Studio is a Los Angeles-based architecture and interior design firm.
+                    <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-7/12 self-end md:self-center max-w-[450px]">FOKO Studio is a Los Angeles-based architecture and interior design firm.
                         Our studio specializes in crafting bespoke, high-end residential and commercial
                         spaces that are as thoughtful as they are visually compelling.
                     </p>
 
-                    <p className="mx-[30px] xl:mx-[50px] hidden xl:block w-7/12 self-end text-16 max-w-[450px] my-foko">
+                    <p className="mx-[30px] xl:mx-[50px] hidden xl:block w-7/12 self-end md:self-center text-16 max-w-[450px] my-foko">
                         At FOKO Studio, we are deeply committed to our clients' specific needs.
                         From intimate interiors to large-scale architectural projects,
                         we combine creative problem-solving with a holistic design approach,
@@ -21,7 +21,7 @@ const AboutUs = () => {
                         we design environments that are perfectly tailored to the unique lifestyle and
                         aspirations of our clients.
                     </p>
-                    <h2 className="font-rubik w-7/12 xl:max-w-[450px] self-end mx-[30px] xl:mx-[50px] xl:justify-self-start text-10 xl:text-20 mt-foko">MORE &nbsp; &nbsp; &#62;</h2>
+                    <h2 className="font-rubik w-7/12 max-w-[450px] self-end md:self-center mx-[30px] xl:mx-[50px] xl:justify-self-start text-10 xl:text-20 mt-foko">MORE &nbsp; &nbsp; &#62;</h2>
                 </span>
             </div>
             <img src={aboutUsImage} alt={'aboutUsImage'} className="filter grayscale mb-foko xl:m-foko"/>
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -5,7 +5,7 @@ const ContactUs = () => {
 
             <div className='flex w-full text-justify'>
                 <h1 className="text-16 font-rubik block xl:hidden mx-[30px]">Contact <br/> Us</h1>
-                <p className="mx-[30px] xl:mx-[50px] w-full xl:max-w-[450px] text-10 xl:text-16">FOKO
+                <p className="mx-[30px] xl:mx-[50px] w-full max-w-[450px] md:self-center text-10 xl:text-16">FOKO
                     Studio is here to bring your architectural dreams to life. We invite you to get in touch with us to
                     discuss your project ideas and see how we can assist you. Email us at contact@foko.studio or give us
                     a call at +123-456-7890. Our team is ready to provide you with the expertise and support you need to
