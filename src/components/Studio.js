1a0957547ea41b21565d98dcd2b227f20a51a13b
d.sajedi <acnt.dv@gmail.com>
2025-03-12 19:15:51 +0330

    studio improvement

--- src/components/Studio.js
+++ src/components/Studio.js
@@ -3,21 +3,21 @@ import aboutUsImage from '../statics/png/about-us.png';
 
 const AboutUs = () => {
     return (
-        <div className="flex flex-col w-full h-full items-center justify-center mt-[200px]">
+        <div className="flex flex-col w-full h-full items-center justify-center mt-0 md:mt-[200px]">
             <div className="flex flex-col-reverse md:flex-row w-full">
 
                 <div className="flex-col w-full md:w-1/2 h-full items-center justify-center">
                     <div className='flex'>
-                        <h1 className="text-16 font-rubik block absolute left-[30px] mt-foko">About <br/> FOKO
+                        <h1 className="text-16 font-rubik block absolute left-[30px] text-left mt-[30px] xl:mt-foko">About <br/> FOKO
                         </h1>
-                        <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
+                        <span className="flex flex-col w-full justify-center items-center text-justify my-[30px] xl:my-foko">
                             <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-1/2 self-end md:self-center ">
                                 FOKO Studio is a Los Angeles-based architecture and interior design firm.
                                 Our studio specializes in crafting bespoke, high-end residential and commercial
                                 spaces that are as thoughtful as they are visually compelling.
                             </p>
 
-                            <p className="mx-[30px] xl:mx-[50px] w-1/2 self-end md:self-center text-10 xl:text-16  my-foko">
+                            <p className="mx-[30px] xl:mx-[50px] w-1/2 self-end md:self-center text-10 xl:text-16 my-foko">
                                 At FOKO Studio, we are deeply committed to our clients' specific needs.
                                 From intimate interiors to large-scale architectural projects,
                                 we combine creative problem-solving with a holistic design approach,
@@ -28,7 +28,7 @@ const AboutUs = () => {
                         </span>
                     </div>
                     <div className='flex'>
-                        <h1 className="text-16 font-rubik block absolute left-[30px] mt-foko">OUR <br/> PHILOSOPHY
+                        <h1 className="text-16 font-rubik block absolute left-[30px] text-left mt-foko">OUR <br/> PHILOSOPHY
                         </h1>
                         <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
                             <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-1/2 self-end md:self-center">
@@ -38,13 +38,13 @@ const AboutUs = () => {
                     </div>
                 </div>
                 <div className="flex w-full md:w-1/2">
-                    <img src={aboutUsImage} alt={'aboutUsImage'} className="w-fit h-fit mb-foko xl:m-foko"/>
+                    <img src={aboutUsImage} alt={'aboutUsImage'} className="w-fit h-fit mb-0 xl:mb-foko xl:m-foko"/>
                 </div>
             </div>
             <div className='flex w-full mb-[100px]'>
-                <h1 className="text-16 font-rubik block absolute left-[30px] mt-foko">OUR <br/> TEAM</h1>
+                <h1 className="text-16 font-rubik block absolute left-[30px] text-left mt-foko">OUR <br/> TEAM</h1>
                 <span className="flex flex-col w-full justify-center items-center text-justify my-foko">
-                    <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-9/12 self-end md:self-center">
+                    <p className="mx-[30px] xl:mx-[50px] text-10 xl:text-16 w-1/2 md:w-9/12 self-end md:self-center">
                         FOKO Studio was founded by Jenny Fontenot and Pegah Koulaeien, two seasoned design professionals with a shared passion for creating inspiring spaces. With diverse backgrounds in architecture and interior design, Jenny and Pegah bring a unique blend of technical expertise and creative intuition to every project. Their collaboration fosters a dynamic, multidisciplinary approach that excels across projects of all scales.
                     </p>
                 </span>
